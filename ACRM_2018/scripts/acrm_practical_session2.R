9b0d399795bbd78beac9483453a38bec407ad675
Keith Lohse <kelopelli@gmail.com>
2019-11-03 20:04:11 -0700

    Adding a nonlinear model
    I have added the code for a negative exponential model to the script file for session 2.

--- ACRM_2018/scripts/acrm_practical_session2.R
+++ ACRM_2018/scripts/acrm_practical_session2.R
@@ -12,13 +12,13 @@ install.packages("dplyr"); install.packages("lmerTest");
 ##----------------------- Data Cleaning and QA ---------------------------------
 ## Setting the Directory -------------------------------------------------------
 getwd()
-setwd("C:/Users/USERNAME/Box Sync/Collaboration/")
+setwd("C:/Users/kelop/Documents/GitHub/LMER_Clinical_Science/ACRM_2018/")
 list.files()
 # Make sure that the file data_session2.csv is saved in your working directory.
 
 # Import the .csv file into R. 
 # We will save this file in the R environment as an object called "DATA".
-DATA<-read.csv("./data_session2.csv", header = TRUE, sep=",",  
+DATA<-read.csv("./data/data_session2.csv", header = TRUE, sep=",",  
                na.strings=c("NA","NaN"," ",""))
 
 # Use the head() function to check the structure of the data file.
@@ -201,11 +201,11 @@ anova(cond_01, cond_02, cond_03)
 
 # Checking the Assumptions of our Best Fitting Model
 ## Level 1 Assumptions ----
-plot(fitted(cond_01),resid(cond_01), ylim=c(-20,20))
+plot(fitted(cond_01),resid(cond_01)/sd(resid(cond_01)), ylim=c(-5,5))
 
 # Normality
-qqnorm(resid(cond_01))
-
+qqnorm(resid(cond_01)/sd(resid(cond_01)))
+abline(0,1)
 
 # Homoscedasticity 
 # Comparable Variances at Level 1
@@ -289,3 +289,85 @@ plot(x=LVL2$subID, y=LVL2$STD_year_cu,
 # variables we created in a new .csv file.
 
 write.csv(DATA, "./data_session3.csv")
+
+
+
+
+
+
+
+
+## ------------- Fitting a Truly Non-Linear Model ------------------------------
+head(DATA)
+
+library(nlme);
+
+MEANS <- DATA %>%
+  group_by(AIS_grade, month) %>%
+  summarize(rasch_FIM = mean(rasch_FIM, na.rm=TRUE))
+head(MEANS)
+
+g1<-ggplot(DATA, aes(x = month, y = rasch_FIM)) +
+  geom_line(aes(group=subID, col=AIS_grade), alpha=0.4)+
+  geom_line(data=MEANS, aes(col=AIS_grade), lwd=1.25)+
+  scale_color_manual(values=c("#1b9e77", "#d95f02", "#7570b3"))
+g2<-g1+scale_x_continuous(name = "Months") +
+  scale_y_continuous(name = "Rasch Scaled FIM")
+g3 <- g2 + theme_classic() + 
+  labs(color = "Group") +
+  theme(axis.text=element_text(size=14, colour="black"), 
+        axis.title=element_text(size=14,face="bold"), 
+        legend.text=element_text(size=14,face="bold"),
+        legend.title=element_text(size=14,face="bold"),
+        strip.text.x = element_text(size = 14), 
+        legend.position="right")
+
+print(g3)
+
+# Because nonlinear models use an iterative testing procedure to find 
+# parameter values that reduce the deviance, we will want to set a "seed"
+# so that we get consistent results.
+set.seed(100)
+
+# Additionally, in order to ensure our nonlinear model converge, we need to set
+# starting values for our different parameters. These starting values can often
+# be determined from a visual inspection of the data or from summary statistics,
+# see Pineheiro & Bates. 
+
+## E.g., start = c(75, -30, -50, 10, -0.3, 0),
+# Where:
+# The first value is an estimate for the asmyptote
+# The second value is an estimate for the group difference in asymptote
+# The third value is an estimate of the change (asymptote to psuedo intercept)
+# The fourth value is an estimate of the group difference in change
+# The fifth values is an estimate of the rate parameter
+# The sixth value is an estimate of the group difference in the rate parameter.
+
+head(DATA)
+
+DATA$Group_C14<-ifelse(DATA$AIS_grade =="C1-4",
+                       1, # Returns if above is TRUE
+                       0)
+
+DATA$Group_C58<-ifelse(DATA$AIS_grade =="C5-8",
+                       1, # Returns if above is TRUE
+                       0)
+
+# Note that this means that paraplegia is the reference group in our model below
+
+neg_exp_rand_mod <- nlme(rasch_FIM~b_1i+b_2*Group_C14+b_3*Group_C58+
+                           (b_4+b_5*Group_C14+b_6*Group_C58)*(exp(b_7i*month+b_8*Group_C14*month+b_9*Group_C58*month)),
+                         data = DATA,
+                         fixed = b_1i + b_2 + b_3 + b_4 + b_5 + b_6 + b_7i + b_8 + b_9 ~ 1,
+                         random = b_1i + b_7i ~ 1,
+                         groups = ~ subID,
+                         start = c(75, -30, -15, -50, 10, 5, -0.3, 0, 0),
+                         na.action = na.omit)
+summary(neg_exp_rand_mod)
+
+
+
+AIC(cond_03)
+AIC(neg_exp_rand_mod)
+# In our case the cubic conditional model actually provided a better approximation 
+# of out data.
